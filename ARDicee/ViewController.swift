9af6953923bfd3d94f4b78a638d3b79e5310eb84
yuki_maitani <fightzoi1997@gmail.com>
2022-06-27 07:32:41 +0900

    箱を表示

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,14 +19,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let material = SCNMaterial()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
